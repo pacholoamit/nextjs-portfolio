b9895dfad48f20ef2d2fdac14deb2fce8f7ba9de
pacholoamit <pacholoamit.tech@gmail.com>
2024-06-01 18:10:21 +0800

    feat: Add TypewriterEffect component for text animation

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -12,14 +12,8 @@ const Hero = () => {
        *  Link: https://ui.aceternity.com/components/spotlight
        */}
       <div>
-        <Spotlight
-          className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
-          fill="white"
-        />
-        <Spotlight
-          className="h-[80vh] w-[50vw] top-10 left-full"
-          fill="purple"
-        />
+        <Spotlight className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen" fill="white" />
+        <Spotlight className="h-[80vh] w-[50vw] top-10 left-full" fill="purple" />
         <Spotlight className="left-80 top-28 h-[80vh] w-[50vw]" fill="blue" />
       </div>
 
@@ -43,7 +37,7 @@ const Hero = () => {
       <div className="flex justify-center relative my-20 z-10">
         <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
           <p className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
-            Dynamic Web Magic with Next.js
+            Software Developer | Devops Engineer | Homelab Enthusiast | Server Administrator
           </p>
 
           {/**
@@ -52,20 +46,16 @@ const Hero = () => {
            *  change md:text-6xl, add more responsive code
            */}
           <TextGenerateEffect
-            words="Transforming Concepts into Seamless User Experiences"
+            words="Engineering efficient and scalable solutions for modern web, mobile, and server applications"
             className="text-center text-[40px] md:text-5xl lg:text-6xl"
           />
 
           <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
-            Hi! I&apos;m Adrian, a Next.js Developer based in Croatia.
+            Hi! I&apos;m Pacholo, a Software Developer based in Cebu, Philippines.
           </p>
 
           <a href="#about">
-            <MagicButton
-              title="Show my work"
-              icon={<FaLocationArrow />}
-              position="right"
-            />
+            <MagicButton title="Show my work" icon={<FaLocationArrow />} position="right" />
           </a>
         </div>
       </div>
